1bae780b5687212bd0d3351cf318a98d5cb1b114
Micaela Corolaire <corolairem@gmail.com>
2023-08-27 22:47:56 -0300

    app with crud system

---
+++ src/database.py
@@ -0,0 +1,15 @@
+# its going to contain the connection with database
+import _mysql_connector #import module to connection
+import mysql.connector
+#data access to connect to database
+
+database=_mysql_connector(
+    host="localhost",
+    user="root",
+    password="3306 ",
+    database="u411959762 python bdd "
+    
+    
+    
+    
+)
